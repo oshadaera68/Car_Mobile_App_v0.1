96af62e2b577646568bb8c82a45c29caf1f2b70c
oshadaera68 <mindartlk010@gmail.com>
2022-10-03 23:44:05 +0530

    updated content

--- screen/Login.js
+++ screen/Login.js
@@ -11,13 +11,14 @@ function Login( {navigation} ) {
         </Text>
         <Input size="md" placeholder="User Name" top="130px" width="75%" />
         <Input size="md" type="password" placeholder="Password" top="160px" width="75%" />
-        <Button size="sm" width="30%" height="20%" top="190px" onPress={()=>{navigation.navigate('Home');}}>
+        <Button size="sm" width="20%" height="40px" top="190px">
           Log In
         </Button>
-        <Text fontSize="xs" color="#000000" top="300px"> Do You haven't an account ? </Text>
-        <Button size="sm" width="30%" height="20%" top="310px" onPress={()=>{navigation.navigate('Signin');}}>
+        <Text fontSize="sm" color="#000000" top="300px"> Do You haven't an account ? </Text>
+        <Button size="sm" width="20%" height="40px" top="310px" onPress={()=>{navigation.navigate('Sign in');}}>
           Sign In
         </Button>
+        <Text italic fontSize="xs" top="400px"> Car App v0.1 </Text>
       </Box>
     </NativeBaseProvider>
   );
--- screen/Signin.js
+++ screen/Signin.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Box, NativeBaseProvider, Text } from 'native-base';
+import { Box, NativeBaseProvider, Text, Input, Button } from 'native-base';
 
-function Signin() {
+function Signin({ navigation }) {
     return (
         <NativeBaseProvider>
             <Box alignItems="center" >
-                <Text>Sign in</Text>
+                <Text fontSize="xl" top="30%">Sign in</Text>
+                <Input size="md" placeholder="id" top="130px" width="75%" />
+                <Input size="md" placeholder="name" top="157px" width="75%" />
+                <Input size="md" placeholder="email" top="187px" width="75%" />
+                <Input size="md" placeholder="address" top="217px" width="75%" />
+                <Input size="md" placeholder="contact" top="247px" width="75%" />
+                <Button size="sm" width="30%" height="40px" top="267px" onPress={() => { navigation.navigate('Home'); }}>
+                    Sign in
+                </Button>
+                <Text italic fontSize="xs" top="100%">Car App v0.1</Text>
             </Box>
         </NativeBaseProvider>
     );
