231a95d01a92517615bcc27968f450d2f2b0d5c8
oshadaera68 <mindartlk010@gmail.com>
2022-10-04 22:14:03 +0530

    updated content

--- screen/Home.js
+++ screen/Home.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import { Box, NativeBaseProvider, Text } from 'native-base';
+import { NativeBaseProvider, Text, Box } from 'native-base';
+
+import { React } from 'react';
+import { Image, StyleSheet } from 'react-native';
 
 function Home() {
     return (
         <NativeBaseProvider>
-            <Box alignItems="center">
-                <Text fontSize="xl" color="#000000" top="67px">Home</Text>
-            </Box>
+            <Box alignItems="center" top="5%"><Text fontSize="sm">Vehicle List</Text></Box>
+            <Image style={styles.car1} source={require('../assets/1.jpg')} />
+            <Box alignItems="center" top="1px"><Text fontSize="sm">Toyota Axio</Text></Box>
+            <Image style={styles.car1} source={require('../assets/2.jpg')} />
+            <Box alignItems="center" top="1px"><Text fontSize="sm">Honda Vezel</Text></Box>
         </NativeBaseProvider>
     );
 }
+
+const styles = StyleSheet.create({
+    car1: {
+        width: 105,
+        height: 83,
+        top:60,
+        left:12
+    }
+});
+
 export default Home;
--- screen/Login.js
+++ screen/Login.js
@@ -1,21 +1,55 @@
 /* eslint-disable no-undef */
-import { Box, NativeBaseProvider, Text, Input, Button } from 'native-base';
-import React from 'react';
+import { Box, NativeBaseProvider, Text, Input, Button, Alert } from 'native-base';
+import { React, useState } from 'react';
+// import { Alert } from 'react-native';
 
-function Login( {navigation} ) {
+function Login({ navigation }) {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const checkData = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: userName,
+        body: password,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => {
+        if (userName === '' && password === '') {
+          Alert.alert('Please fill..');
+        } else if (userName !== '' && password !== '') {
+          console.log(response);
+          Alert.alert('Log in Success..!');
+        } else {
+          if (userName === '') {
+            Alert.alert('Please fill user name..!');
+          } else if (password === '') {
+            Alert.alert('Please Fill password...!');
+          }
+        }
+      })
+      .catch(_err => {
+        Alert.alert('Error occured..!');
+      });
+  };
+
   return (
     <NativeBaseProvider>
       <Box alignItems="center">
         <Text fontSize="xl" color="#000000" top="100px">
           Log In
         </Text>
-        <Input size="md" placeholder="User Name" top="130px" width="75%" />
-        <Input size="md" type="password" placeholder="Password" top="160px" width="75%" />
-        <Button size="sm" width="20%" height="40px" top="190px">
+        <Input size="md" value={userName} onChangeText={e => { setUserName(e); }} placeholder="User Name" top="130px" width="75%" />
+        <Input size="md" value={password} onChangeText={e => { setPassword(e); }} type="password" placeholder="Password" top="160px" width="75%" />
+        <Button size="sm" width="20%" height="40px" top="190px" onPress={() => { checkData }}>
           Log In
         </Button>
         <Text fontSize="sm" color="#000000" top="300px"> Do You haven't an account ? </Text>
-        <Button size="sm" width="20%" height="40px" top="310px" onPress={()=>{navigation.navigate('Sign in');}}>
+        <Button size="sm" width="20%" height="40px" top="310px" onPress={() => { navigation.navigate('Sign in'); }}>
           Sign In
         </Button>
         <Text italic fontSize="xs" top="400px"> Car App v0.1 </Text>
